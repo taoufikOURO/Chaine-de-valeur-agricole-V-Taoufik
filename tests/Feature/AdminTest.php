378590931edf950bfe90c4561b33e5f045a078e5
taoufikOURO <ourotaoufik1@gmail.com>
2025-04-28 14:11:41 +0000

    tests de global stats et culture

---
+++ tests/Feature/AdminTest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Culture;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class AdminTest extends TestCase
+{
+    public function test_admin_can_access_global_stats()
+    {
+        $admin = User::where(['email' => 'xiyim14198@exclussi.com'])->first();
+        $admin->email_verified_at = now();
+        $admin->save();
+        $response = $this->actingAs($admin)->get(route('stats.global'));
+
+        // Vérifier qu'il a bien accès (statut HTTP 200)
+        $response->assertOk();
+    }
+    public function test_admin_can_access_culture_index()
+    {
+        $admin = User::where(['email' => 'xiyim14198@exclussi.com'])->first();
+        $admin->email_verified_at = now();
+        $admin->save();
+
+        $response = $this->actingAs($admin)->get(route('culture.index'));
+
+        $response->assertOk();
+    }
+    public function test_admin_can_access_culture_create_form()
+    {
+        $admin = User::where('email', 'xiyim14198@exclussi.com')->first();
+        $admin->email_verified_at = now();
+        $admin->save();
+
+        $response = $this->actingAs($admin)->get(route('culture.create'));
+
+        $response->assertOk();
+    }
+
+    /* Ce test peut ne pas marcher si le random donne le même nom de culture .... le nom est unique donc.... */
+    public function test_admin_can_create_culture()
+    {
+        $admin = User::where('email', 'xiyim14198@exclussi.com')->first();
+        $admin->email_verified_at = now();
+        $admin->save();
+        $data = [
+            'nom' => Str::random(10),
+            'type_culture_id' => 1,
+        ];
+
+        $response = $this->actingAs($admin)->post(route('culture.store'), $data);
+        $response->assertRedirect(route('culture.index'));
+    }
+
+    public function test_admin_can_access_edit_culture_page()
+    {
+        $admin = User::where('email', 'xiyim14198@exclussi.com')->first();
+
+        $culture = Culture::create([
+            'code' => Str::uuid()->toString(),
+            'nom' => Str::random(10),
+            'type_culture_id' => 1,
+        ]);
+        $response = $this->actingAs($admin)->get(route('culture.edit', ['culture' => Crypt::encrypt($culture->id)]));
+
+        $response->assertOk();
+    }
+    public function test_admin_can_update_culture()
+    {
+        $admin = User::where('email', 'xiyim14198@exclussi.com')->first();
+        // Créer une culture existante
+        $culture = Culture::create([
+            'code' => Str::uuid()->toString(),
+            'nom' => Str::random(10),
+            'type_culture_id' => 1,
+        ]);
+
+        $updatedData = [
+            'code' => Str::uuid()->toString(),
+            'nom' => Str::random(10),
+            'type_culture_id' => 1,
+        ];
+
+        $response = $this->actingAs($admin)->put(route('culture.update', $culture->id), $updatedData);
+
+        $response->assertRedirect(route('culture.index'));
+    }
+    public function test_admin_can_delete_culture()
+    {
+        $admin = User::where('email', 'xiyim14198@exclussi.com')->first();
+        $culture = Culture::create([
+            'code' => '1234567890',
+            'nom' => 'Ma Culture à Supprimer',
+            'type_culture_id' => 1,
+        ]);
+
+        $response = $this->actingAs($admin)->delete(route('culture.destroy', $culture->id));
+
+        $response->assertRedirect(route('culture.index'));
+    }
+}
